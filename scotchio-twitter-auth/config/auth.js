ef2399ec6e8161072f569300b8af1277416d79f0
Erkanderon <erkan.cetinkaya3@gmail.com>
2016-10-26 16:01:33 +0300

    auth examples added

---
+++ scotchio-twitter-auth/config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : 'your-secret-clientID-here', // your App ID
+        'clientSecret'  : 'your-client-secret-here', // your App Secret
+        'callbackURL'   : 'http://facebook.com/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-secret-clientID-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://127.0.0.1:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
